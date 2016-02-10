06f457f738f17537ccaaa8077838977cffba0d9a
William Chan <willchan@gmail.com>
2016-02-10 23:58:57 +0000

    A bunch of fixes to get it to work for insouciant.org.
    - Oops, screwed up the webroot. Used the parent directory via
      path.Dir(). Deleted that.
    - Let's Encrypt webroot DV requires port 80 support. Add it. Redirect
      everything to port 443.

--- pandaweb/main.go
+++ pandaweb/main.go
@@ -5,10 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
-	"path"
+	"strconv"
+	"strings"
 	"syscall"
 
 	"github.com/gorilla/handlers"
@@ -16,21 +18,45 @@ import (
 )
 
 var (
-	port     = flag.Int("port", -1, "port to listen to")
-	webRoot  = flag.String("webroot", "", "root directory for web file serving")
-	certFile = flag.String("certfile", "", "path to certificate chain file")
-	keyFile  = flag.String("keyfile", "", "path to private key file")
+	httpsPort = flag.Int("https_port", -1, "https port to listen to")
+	httpPort  = flag.Int("http_port", -1, "http port to listen to, which redirects to the https port")
+	webRoot   = flag.String("webroot", "", "root directory for web file serving")
+	certFile  = flag.String("certfile", "", "path to certificate chain file")
+	keyFile   = flag.String("keyfile", "", "path to private key file")
 )
+
+func redirectHttpHttps(w http.ResponseWriter, r *http.Request) {
+	u := r.URL
+	u.Scheme = "https"
+	if strings.Index(r.Host, ":") != -1 {
+		var err error
+		u.Host, _, err = net.SplitHostPort(r.Host)
+		if err != nil {
+			fmt.Println("err: ", err)
+			http.NotFound(w, r)
+			return
+		}
+	} else {
+		u.Host = r.Host
+	}
+	if *httpsPort != 443 {
+		u.Host = net.JoinHostPort(u.Host, strconv.Itoa(*httpsPort))
+	}
+	http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
+}
 
 func main() {
 	flag.Parse()
 
-	if *port < 0 || *port >= 1<<16 {
-		fmt.Println("-port has invalid port:", *port)
+	if *httpsPort < 0 || *httpsPort >= 1<<16 {
+		fmt.Println("-https_port has invalid port:", *httpsPort)
 		os.Exit(1)
 	}
-	webRootPath := path.Dir(*webRoot)
-	if _, err := os.Stat(webRootPath); os.IsNotExist(err) {
+	if *httpPort < 0 || *httpPort >= 1<<16 {
+		fmt.Println("-http_port has invalid port:", *httpPort)
+		os.Exit(1)
+	}
+	if _, err := os.Stat(*webRoot); os.IsNotExist(err) {
 		fmt.Println("-webroot is invalid path")
 		os.Exit(1)
 	}
@@ -48,15 +74,14 @@ func main() {
 		return cm.GetCertificate(clientHello)
 	}
 
-	http.Handle("/", handlers.LoggingHandler(os.Stdout, http.FileServer(http.Dir(webRootPath))))
-
+	// Start up the HTTPS server.
+	http.Handle("/", handlers.LoggingHandler(os.Stdout, http.FileServer(http.Dir(*webRoot))))
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", *port),
+		Addr: fmt.Sprintf(":%d", *httpsPort),
 		TLSConfig: &tls.Config{
 			GetCertificate: gc,
 		},
 	}
-
 	go func() {
 		if err := srv.ListenAndServeTLS("", ""); err != nil {
 			fmt.Println("Failed to listen and serve:", err)
@@ -64,7 +89,15 @@ func main() {
 		}
 	}()
 
-	fmt.Printf("pandaweb is running on port %d...\n", *port)
+	// Start up the HTTP server and have it redirect to HTTPS.
+	go func() {
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", *httpPort), http.HandlerFunc(redirectHttpHttps)); err != nil {
+			fmt.Println("Failed to listen and serve:", err)
+			os.Exit(1)
+		}
+	}()
+
+	fmt.Printf("pandaweb is running on https port %d and http port %d...\n", *httpsPort, *httpPort)
 
 	for _ = range sigChan {
 		log.Printf("Reloading TLS certificate")
